3e6db9c4a589df45e1b2fe27ae69c36b9a9ac878
PaulB1985 <paulpriest1985@gmail.com>
2021-08-24 19:09:17 +0300

    feat: add guessing game code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        let min = 0;
+        let max = 0;
+        let maybe = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.maybe = Math.ceil( (this.max - this.min) / 2 ) + this.min;
+        // console.log( this.min + ' ' + this.maybe + ' ' + this.max );
+        return this.maybe;
     }
 
     lower() {
-
+        this.max = this.maybe;
     }
 
     greater() {
-
+        this.min = this.maybe;
     }
 }
 
